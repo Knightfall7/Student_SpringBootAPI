bacade068e1046ab71373ffcfba2d61fc99f47e3
Chris_nkuutu <nkuutugeorge@gmail.com>
2024-01-09 15:01:22 +0100

    Add files via upload

---
+++ src/main/java/org/example/studentspringbootrestproject/StudentSpringBootRestApi.java
@@ -0,0 +1,12 @@
+package org.example.studentspringbootrestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSpringBootRestApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentSpringBootRestApi.class, args);
+    }
+}
